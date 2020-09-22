bbdc02f488e6720aa57f19505af706620a19917e
Lucas Talamini <53242571+lucastalamini@users.noreply.github.com>
2020-09-22 11:45:05 -0300

    Update wp-cli.sh

--- wp-cli.sh
+++ wp-cli.sh
@@ -17,8 +17,8 @@
 
     if [[ "$USER" == "root" ]]; then 
         wget -q  https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /tmp/wp-cli.phar
-        chmod +x wp-cli.phar 
-        mv -v wp-cli.phar /usr/local/bin/wp
+        chmod +x /tmp/wp-cli.phar 
+        mv -v /tmp/wp-cli.phar /usr/local/bin/wp
         echo "WP-CLI installed successfully." 
     else
         echo "You must be root to do this."
