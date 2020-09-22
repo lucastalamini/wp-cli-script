acc21ab1dc76f537081cb44edd001e78debf3054
Lucas Talamini <53242571+lucastalamini@users.noreply.github.com>
2020-09-22 11:37:15 -0300

    Update wp-cli.sh

--- wp-cli.sh
+++ wp-cli.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-############################################################
-#                                                          #
-# Name: wp-cli.sh                                          #
-#                                                          #
-# Author: Lucas Talamini (lucastalamini1994@gmail.com)     #
-# Date: 21/09/2020                                         #
-#                                                          #
-# Description: The script installs wp-cli in a given       #
-#              directory.                                  #
-#                                                          #
-#                                                          #
-# Use: ///                                                 #
-#                                                          #
-############################################################
+########################################################################################################################################################
+#                                                                                                                                                      #
+# Name: wp-cli.sh                                                                                                                                      #
+#                                                                                                                                                      #
+# Author: Lucas Talamini (lucastalamini1994@gmail.com)                                                                                                 #
+# Date: 21/09/2020                                                                                                                                     #
+#                                                                                                                                                      #
+# Description: The script installs wp-cli in a given directory.                                                                                        #
+#                                                                                                                                                      #
+#                                                                                                                                                      #
+#                                                                                                                                                      #
+# Use: bash <(curl -sk https://raw.githubusercontent.com/lucastalamini/wp-cli-script/master/wp-cli.sh)                                                 #
+#                                                                                                                                                      #
+########################################################################################################################################################
 
     if [[`whoami` = 'root']]; then 
         wget -q  https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /tmp/wp-cli.phar
