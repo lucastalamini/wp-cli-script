8f0346e0a289839421ec3e7b4c927bd86dc8846e
Lucas Talamini <53242571+lucastalamini@users.noreply.github.com>
2020-09-22 11:34:01 -0300

    Create wp-cli.sh

---
+++ wp-cli.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+############################################################
+#                                                          #
+# Name: wp-cli.sh                                          #
+#                                                          #
+# Author: Lucas Talamini (lucastalamini1994@gmail.com)     #
+# Date: 21/09/2020                                         #
+#                                                          #
+# Description: The script installs wp-cli in a given       #
+#              directory.                                  #
+#                                                          #
+#                                                          #
+# Use: ///                                                 #
+#                                                          #
+############################################################
+
+    if [[`whoami` = 'root']]; then 
+        wget -q  https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /tmp/wp-cli.phar
+        chmod +x wp-cli.phar 
+        mv -v wp-cli.phar /usr/local/bin/wp
+        echo "WP-CLI installed successfully." 
+    else
+        echo "You must be root to do this."
+        exit
+    fi
